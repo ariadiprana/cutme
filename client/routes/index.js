7049c4861f0f360b0b4d25cb8df4c0e933d96345
digachandra <imdigachandra@gmail.com>
2016-10-06 16:47:42 +0700

    <client> Create signin page route & interface

--- client/routes/index.js
+++ client/routes/index.js
@@ -2,13 +2,21 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next) {
-  //res.render('master')
   var locals = {
     title: 'Page Title',
     description: 'Page Description',
     header: 'Page Header'
   };
-  res.render('pages/home', locals);
+  res.render('pages/signin', locals);
+})
+
+router.get('/signup', function(req, res, next) {
+  var locals = {
+    title: 'Page Title',
+    description: 'Page Description',
+    header: 'Page Header'
+  };
+  res.render('pages/signup', locals);
 })
 
 module.exports = router
